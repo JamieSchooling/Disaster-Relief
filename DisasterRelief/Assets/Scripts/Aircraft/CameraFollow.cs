69536dcd3f4390ea284cf77b291436167d2ecafc
ThatQuantumGoose <81333437+ThatQuantumGoose@users.noreply.github.com>
2022-05-05 09:32:09 +0100

    Update CameraFollow.cs
    Ye

--- DisasterRelief/Assets/Scripts/Aircraft/CameraFollow.cs
+++ DisasterRelief/Assets/Scripts/Aircraft/CameraFollow.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player;
-    public Vector3 positionOffset = Vector3.zero;
-    public Vector3 rotationOffset = Vector3.zero;
+    [SerializeField]
+    Transform player;
+
+    [SerializeField]
+    Vector2 positionOffset = Vector2.zero;
+
+    [SerializeField]
+    Vector3 rotationOffset = Vector3.zero;
+
+    [SerializeField]
+    float yOffset;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +25,8 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.TransformPoint(positionOffset);
+        transform.position = player.TransformPoint(new Vector3(positionOffset.x, 0, positionOffset.y));
+        transform.position = transform.position + new Vector3(0, yOffset, 0);
         transform.eulerAngles = new Vector3(player.transform.eulerAngles.x + rotationOffset.x, player.transform.eulerAngles.y + rotationOffset.y, rotationOffset.z);
     }
 }
