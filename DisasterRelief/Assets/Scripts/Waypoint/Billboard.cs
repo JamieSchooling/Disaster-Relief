a9208dc66f131ea4c6fd2eea8d8c64b5cab77517
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2022-06-16 10:02:20 +0100

    Added comments

--- DisasterRelief/Assets/Scripts/Waypoint/Billboard.cs
+++ DisasterRelief/Assets/Scripts/Waypoint/Billboard.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
+    // Reference to the player camera
     [SerializeField] Transform playerCam;
 
+    // Late update delays movement until after the plane has moved to prevent glitches
     void LateUpdate()
     {
+        // Makes the billboard face the camera
         transform.LookAt(transform.position + playerCam.transform.rotation * Vector3.forward, playerCam.transform.rotation * Vector3.up);
+
+        // Locks the rotation to the y-axis
         Vector3 eulerAngles = transform.eulerAngles;
         eulerAngles.z = 0;
         eulerAngles.x = 0;
 
+        // Applies the rotation
         transform.eulerAngles = eulerAngles;
     }
 }
